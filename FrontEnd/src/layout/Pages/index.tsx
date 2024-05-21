98a1f5282369c707fe7e2cec3b14542ce202fd0f
ArielDveirin <ariel.private2@Gmail.com>
2024-05-21 18:01:17 +0300

    delete old / moved files

--- FrontEnd/src/layout/Pages/index.tsx
+++
@@ -1,9 +0,0 @@
-import { Outlet } from 'react-router-dom';
-
-// ==============================|| LAYOUT - BLANK PAGES ||============================== //
-
-const PagesLayout = () => {
-  return <Outlet />;
-};
-
-export default PagesLayout;
